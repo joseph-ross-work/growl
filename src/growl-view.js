cd8805e949c7be09a5b98b459e78cf1f51880a2a
Joseph Ross <jross@livefyre.com>
2014-11-10 11:08:22 -0800

    Growl now creates its own psc unless otherwise told not to and can now take classes to use for the in and out animations

--- src/growl-view.js
+++ src/growl-view.js
@@ -1,7 +1,8 @@
 define([
     'growl/package-attribute',
+    './personalized-stream-client',
     'less!../src/css/styles'
-], function (PackageAttribute) {
+], function (PackageAttribute, PersonalizedStreamClient) {
     'use strict';
 
     var MAIN_TEMPLATE = '<div class="growl-box lf">'+
@@ -13,14 +14,19 @@ define([
 
 
     var growlButton, growlBox, growlMsg, growlTags;
-    var inDelay, inInterval, outDelay, outInterval;
+    var inDelay, inInterval, inAnimationClass, outDelay, outInterval, outAnimationClass;
     var msgQueue = [];
 
     var GrowlView = function (opts) {
         opts = opts || {};
         this.el = opts.el;
+        this._psc = opts.personalizedStreamClient !== undefined ? opts.personalizedStreamClient : new PersonalizedStreamClient(opts);
+        this._showMessages = opts.initializePaused !== undefined ? !opts.initializePaused : true;
+        
         inDelay = opts.inDelay || 500;
         outDelay = opts.outDelay || 4000;
+        inAnimationClass = opts.inAnimationClass || 'growl-show';
+        outAnimationClass = opts.outAnimationClass || 'growl-hide';
 
         PackageAttribute.decorate(this.el);
         this.el.innerHTML = MAIN_TEMPLATE;
@@ -33,21 +39,33 @@ define([
         growlMsg = this.el.getElementsByClassName('growl-message')[0];
 
         growlButton.onclick = function(event) {
-            if(inInterval) clearInterval(inInterval);
-            if(outInterval) clearInterval(outInterval);
+            if (inInterval) { 
+                clearInterval(inInterval);
+            }
+            if (outInterval) {
+                clearInterval(outInterval);
+            }
             inInterval = undefined;
             outInterval = undefined;
 
-            growlBox.classList.remove('growl-show');
-            growlBox.classList.add('growl-hide');
+            growlBox.classList.remove(inAnimationClass);
+            growlBox.classList.add(outAnimationClass);
         };
     };
 
-    GrowlView.prototype.onPostMessage = function(event){
+    GrowlView.prototype.pause = function() {
+        this._showMessages = false;
+    }
+
+    GrowlView.prototype.play = function() {
+        this._showMessages = true;
+    }
+
+    GrowlView.prototype.onPostMessage = function(event) {
         var msg = typeof event.data === "string" ?
             JSON.parse(event.data) : event.data;
 
-        if(msg.channel === 'personalized-stream'){
+        if (msg.channel === 'personalized-stream' && this._showMessages) {
             if(msg.topic === 'content'){
                 msgQueue.push(msg.data)
                 this.showMessage();
@@ -55,14 +73,17 @@ define([
         }
     };
 
-    GrowlView.prototype.getTagsHtml = function(activity){
-        if(!activity || !activity.object || !activity.object.tags) return;
+    GrowlView.prototype.getTagsHtml = function(activity) {
+        if (!activity || !activity.object || !activity.object.tags) { 
+            return;
+        }
 
         var tags = activity.object.tags;
         var tagsString = '';
 
-        for(var i = 0; i < tags.length; i++){
-            if(!tags[i].displayName) continue;
+        for(var i = 0; i < tags.length; i++) {
+            if(!tags[i].displayName) 
+                continue;
             tagsString += '<li class="growl-tag"><span class="growl-tag-text">' +
                 tags[i].displayName + '</span></li>';
         }
@@ -70,32 +91,38 @@ define([
         return tagsString;
     };
 
-    GrowlView.prototype.showMessage = function(){
+    GrowlView.prototype.showMessage = function() {
         var self = this;
 
-        if(inInterval || outInterval) return;
+        if (inInterval || outInterval) {
+            return;
+        }
+
         var activity = msgQueue.length > 0 ? msgQueue.shift() : null;
-        if(!activity) return;
+        
+        if (!activity) {
+            return;
+        }
 
         var text = activity.object.title || ' ';
         growlMsg.innerHTML = text;
         growlTags.innerHTML = this.getTagsHtml(activity);
 
         var myInInterval;
-        inInterval = setInterval(function(){
+        inInterval = setInterval(function() {
             if(outInterval) clearInterval(outInterval);
             clearInterval(myInInterval);
             inInterval = undefined;
 
-            growlBox.classList.remove('growl-hide');
-            growlBox.classList.add('growl-show');
+            growlBox.classList.remove(outAnimationClass);
+            growlBox.classList.add(inAnimationClass);
             
             var myOutInterval;
-            outInterval = setInterval(function(){
+            outInterval = setInterval(function() {
                 clearInterval(myOutInterval);
                 outInterval = undefined;
-                growlBox.classList.add('growl-hide');
-                growlBox.classList.remove('growl-show');
+                growlBox.classList.add(outAnimationClass);
+                growlBox.classList.remove(inAnimationClass);
                 self.showMessage();
             }, outDelay);
             myOutInterval = outInterval;
@@ -103,8 +130,10 @@ define([
         myInInterval = inInterval;
     };
 
-    GrowlView.prototype.destroy = function(){
+    GrowlView.prototype.destroy = function() {
         growlButton.onclick = null;
+        this._psc.destroy();
+        this._psc = null;
         window.removeEventListener('message', this.onPostMessage, false)
     };
 
